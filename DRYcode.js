f46d29a6f6508c4c03c77ffdc4231887e909f8f1
Christine Lovett <clovett07@gmail.com>
2015-07-15 11:38:29 -0400

    added files to improve code

---
+++ DRYcode.js
@@ -0,0 +1,42 @@
+var game = {
+    question1: "Which river flows through Washington, D.C.?",
+    answer1: "Potomac",
+    question2: "What is the Northern most state in the U.S.?",
+    answer2: "Alaska",
+    question3: "**DAILY DOUBLE** In what state is the only rainforest in the Continental United States?",
+    answer3: "Washington",
+    question4: "What historical path extends 400 miles from Natchez, Mississippi to Nashville, TN?",
+    answer4: "Natchez Trace",
+    question5: "Which popular european men's sport is played by predominantely women in the United States",
+    answer5: "Field Hockey",
+    question6: "Who did Novak Djokavic defeat in this years Wimbledon final?",
+    answer6: "Roger Federer",
+    question7: "How many games is Tom Brady suspended for after being found guilty of Deflate-gate?",
+    answer7: "4",
+    question8: "Which NBA basketball team has won the most Championships?",
+    answer8: "Boston Celtics",
+    question9: "Who phoned home in the Steven Spielbergs 80's alien hit?",
+    answer9: "E.T.",
+    question10: "Which male actor played a starring role in two major blockbuster alien movies from the 90s?",
+    answer10: "Will Smith",
+    question11: "How many Terminator movies are there?",
+    answer11: "4",
+    question12: "**DAILY DOUBLE** What year was The X-Files movie released?",
+    answer12: "1998",
+    question13: "Which icon started out as the youngest member of his family band?",
+    answer13: "Michael Jackson",
+    question14: "Who was the lead singer of The Supremes?",
+    answer14: "Diana Ross",
+    question15: "The Four Tops, Marvin Gaye, and The Temptations were all part of this record company.",
+    answer15: "Motown",
+    question16: "Who sings 'My Guy'?",
+    answer16: "Mary Wells",
+}
+
+
+ //  game.initialize();
+ //  initialize: function (){
+ //    alert('example')
+ //  },
+ //
+ // }
